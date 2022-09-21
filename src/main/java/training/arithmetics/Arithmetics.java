be1a9fa2f33b66499bc2ff5ea4d36a8cc03307f1
kubarni <kubarni@stud.uni-obuda.hu>
2022-09-21 11:00:58 +0200

    try with plus

--- src/main/java/training/arithmetics/Arithmetics.java
+++ src/main/java/training/arithmetics/Arithmetics.java
@@ -2,6 +2,6 @@ package training.arithmetics;
 
 public class Arithmetics {
     public int multiply(int firstArgument, int secondArgument) {
-        firstArgument * secondArgument;
+        firstArgument + secondArgument;
     }
 }
